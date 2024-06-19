e39623a79cb55d01eaf5c15b51523c33e6b3743b
mhlee <karyo13234@gmail.com>
2024-06-19 15:59:53 +0900

    state data에 user auth cookis2
    (cherry picked from commit 7b2b0a2bd8ed03939001e8ef0dc734d67a106d14)
    (cherry picked from commit e3f9827bd6c0d4636a622a368099815dfe066947)

--- vue-til/src/store/index.js
+++ vue-til/src/store/index.js
@@ -13,8 +13,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    token: getAuthFromCookie || '',
-    username: getUserFromCookie || '',
+    token: getAuthFromCookie() || '',
+    username: getUserFromCookie() || '',
   },
   getters: {
     isLogin: state => state.username,
